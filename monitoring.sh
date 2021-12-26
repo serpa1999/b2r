88f72af706991983a86a5082f7c78d033c82fdfa
Ryndoon Shor <rshor@ga-i3.msk.21-school.ru>
2021-12-26 15:10:31 +0300

    fin script monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,10 +1,17 @@
 clear
-RAM_total=$(cat /proc/meminfo | grep MemTotal | awk '{ print $2 }')
-RAM_free=$(cat /proc/meminfo | grep MemFree | awk '{ print $2 }')
-MEM_total=$(du / -s 2>/dev/null | awk '{ print $1 }')
-echo "#Architecture: $(uname -a)"
-echo "#CPU physical : $(cat /proc/cpuinfo | grep "cpu cores" | sed -n "1p" | awk '{ print $4 }')"
-echo "#vCPU : $(nproc)"
-echo "#Memory Usage: $(((RAM_total - RAM_free) / 1024))/$((RAM_total / 1024))MB ($(printf %.2f "$((1000000000 *   (RAM_total - RAM_free)/RAM_total  ))e-7")%)"
-echo "#Disk Usage: $((MEM_total/1024))"
-grep 'cpu' /proc/stat
+RAM_total=$(free -m | awk '$1 == "Mem:" {print $2}')
+RAM_used=$(free -m | awk '$1 == "Mem:" {print $3}')
+MEM_total=$(df -Bm | grep '^/dev/' | grep -v 'boot' | awk '{ a += $2 } END { print a }')
+MEM_used=$(df -Bm | grep '^/dev/' | grep -v 'boot' | awk '{ b += $3 } END { print b }')
+wall "	#Architecture: $(uname -a)
+	#CPU physical : $(cat /proc/cpuinfo | grep "physical id" | uniq | wc -l)
+	#vCPU : $(cat /proc/cpuinfo | grep "^processor" | wc -l)
+	#Memory Usage: $RAM_used/$((RAM_total))MB ($(printf %.2f "$(((1000000000 * RAM_used)/RAM_total  ))e-7")%)
+	#Disk Usage: $MEM_used/$((MEM_total/1024))GB ($(printf %.2f "$(((1000000000 * MEM_used)/MEM_total  ))e-7")%)
+	#CPU load: $(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f"), $1 + $3}')%
+	#Last boot: $(who -b | awk '{ print $3 " " $4 }')
+	#LVM use: $(if [ $(lsblk | grep LVM | wc -l) -eq 0 ]; then echo no; else echo yes; fi)
+	#Connnections TCP : $(cat /proc/net/sockstat | awk '$1 == "TCP:" {print $3 " ESTABLISHED"}')
+	#User log: $(users | wc -w)
+	#Network: IP $(hostname -I) ($(ip link show | awk '$1 == "link/ether" {print $2}'))
+	#Sudo : $(journalctl _COMM=sudo | grep COMMAND | wc -l) cmd"
